53039d866c982877db313b3af1ba09a0633d25f7
Jonlhuang <jonalhuang@gmail.com>
2023-11-27 14:20:39 -0800

    correcting input term

--- SSIPM/calculate.prior.R
+++ SSIPM/calculate.prior.R
@@ -5,19 +5,20 @@
 #' @export
 #
 # 
-calculate.prior <- function(Fit,Prior){
+calculate.prior <- function(Cand,Prior){
   
-  # could include error code if length(Fit) != length(Prior)
+  # could include error code if length(Cand) != length(Prior)
+  if(length(Cand) != length(Prior$Names)) stop("Error: input vectors do not have the same length") #error code
   
-  Result = rep(NA,length(Fit))
+  Result = rep(NA,length(Cand))
   
-  for (i in length(Fit)){
+  for (i in 1:length(Cand)){
     if (Prior$Type[i] == 'lognormal'){
       
-      Result[i] = log(dnorm(log(Fit[i]),mean = Prior$Means[i],sd = Prior$SDs[i]))
+      Result[i] = log(dnorm(log(Cand[i]),mean = Prior$Means[i],sd = Prior$SDs[i]))
   
                       } # end if lognormal (currently there are no other options)
-  } # end loop over Fit
+  } # end loop over Cand
   
   Result = sum(Result)
   return(Result)
